92453503807e355ec7410f1b22969bafd01b0978
phoebe_stage <phoebestage01@gmail.com>
2024-10-08 08:40:44 +1100

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,11 @@ import NavBar from '../components/NavBar';
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-dark items-center">
-      <Head>
+
       <NavBar />
         <title>Phoebe Stage</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+
       <main>
         <h1 className="text-light-mid m-10 text-8xl">
           Phoebe Stage
