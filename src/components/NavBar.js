87a07ebd8b550ba38dd2417f27e756903d9ed1f3
phoebe_stage <phoebestage01@gmail.com>
2024-10-08 11:30:32 +1100

    nav bar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import NavBarLink from "./NavBarLink";
 export default function NavBar() {
     return (
       <>
-        <div className="w-full bg-dark flex  justify-end">
+        <div className="w-full p-4 bg-dark flex  justify-end">
 
             <NavBarLink text = "about" link = "" />
             <NavBarLink text = "projects" link = "" />
--- src/components/NavBarLink.js
+++ src/components/NavBarLink.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function NavBarLink({ text, Icon, link }) {
     return (
         <Link href={link}>
-          <div className="flex m-5 text-light-mid">
+          <div className=" w-48 mt-5 mb-5 p-4 text-center text-light-mid hover:bg-light-mid hover:text-dark">
             <p> {text} </p>
           </div>
         </Link>
