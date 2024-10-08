a558b27871aa1cce55f6b460ee5250e85dfad8ed
phoebe_stage <phoebestage01@gmail.com>
2024-10-08 21:41:47 +1100

    phone compatibility

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,11 +6,11 @@ export default function Layout({ children }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
-      <div className="min-h-screen w-full flex flex-col bg-dark overflow-hidden">
-        <div className="flex-grow p-10 overflow-y-auto">
+      <div className="min-h-screen w-full h-svh flex flex-col bg-dark overflow-hidden">
+        <div className="flex-grow p-4 sm:p-10 overflow-y-auto">
           {children}
         </div>
-        <div className="footer flex items-center justify-center mb-10 w-full">
+        <div className="footer fixed bottom-0 flex items-center justify-center mb-10 w-full">
           <NavBar />
         </div>
       </div>
